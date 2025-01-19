d1b1412644626f616b2b831f6c2297ecefc892df
Liliia Rafikova <lika.rafikova.work@gmail.com>
2025-01-19 17:02:35 +0000

    Added custom implementation example of CustomLockWithCondition

---
+++ src/customSynchronizers/CustomLockWithCondition.java
@@ -0,0 +1,17 @@
+package customSynchronizers;
+
+public class CustomLockWithCondition {
+    private boolean isLocked = false;
+
+    public synchronized void lock() throws InterruptedException {
+        while (isLocked) {
+            wait(); // Wait until the lock is available
+        }
+        isLocked = true;
+    }
+
+    public synchronized void unlock() {
+        isLocked = false;
+        notify();  // Notify one waiting thread that the lock is available
+    }
+}
